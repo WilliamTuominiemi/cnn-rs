accd009873d6f01860d879721df1f8f1af0982c8
williamtuominiemi <william.m.tuominiemi@gmail.com>
2026-08-16 18:26:16 +0300

    script for renaming images

---
+++ scripts/rename_images.ps1
@@ -0,0 +1,15 @@
+$location = Get-Location
+$targetPath = Join-Path -Path $location -ChildPath "src\images\raw_images\"
+$folders = Get-ChildItem -Path $targetPath
+
+foreach ($folder in $folders)
+{
+    $folderTargetPath = Join-Path -Path $targetPath -ChildPath $folder
+    $files = Get-ChildItem -Path $folderTargetPath
+    $i = 0
+    foreach ($file in $files) {
+        $newName = "$i.png"
+        Rename-Item -Path $file.FullName -NewName $newName -ErrorAction SilentlyContinue
+        $i++
+    }
+}
