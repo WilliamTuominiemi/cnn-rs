ad74fabc68af5b4335e2490b31be685d551a56fb
williamtuominiemi <william.m.tuominiemi@gmail.com>
2026-08-16 18:04:31 +0300

    restructure folders, first preprocessing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use image::ImageReader;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let img = ImageReader::open("./src/images/raw_images/0/1.png")?.decode()?;
+    let mut processed_img = img.grayscale().adjust_contrast(90.0);
+    let _ = processed_img.invert();
+    processed_img = processed_img.brighten(20).adjust_contrast(40.0);
+    processed_img.save("./src/images/processed_images/0/1.png")?;
+
+    Ok(())
 }
